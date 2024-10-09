1e52eef30d4569aa38e21e6974c3beeaf461526c
PaulTiberiu <iordache.tiberiu19@yahoo.com>
2024-10-09 17:53:45 +0200

    quit button on main menu, death message, horizontal teleportation when passing the borders, replay button, main menu button

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -20,4 +20,18 @@ public class MainMenu : MonoBehaviour
         // Load GameScene
         SceneManager.LoadScene("GameScene");
     }
+
+    public void ExitGame()
+    {
+        // Quit the application
+        #if UNITY_EDITOR
+            // If the game is running in the editor, stop playing the scene
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            // If running in a build, quit the application
+            Application.Quit();
+        #endif
+    }
+
+
 }
