0f42c8a5cef2a4ab972c95cbf4e4eb64738f11fb
Iordache Paul-Tiberiu <iordache.tiberiu19@yahoo.com>
2024-10-29 17:30:49 +0100

    adding break tile sound

--- Assets/Script/BreakingTile.cs
+++ Assets/Script/BreakingTile.cs
@@ -4,17 +4,29 @@ using UnityEngine;
 
 public class BreakingTile : MonoBehaviour
 {
-
     private Animator anim;
+    public AudioSource audioSource; // Reference to the AudioSource
 
     void Awake()
     {
         anim = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>(); // Get the AudioSource component
     }
+
     void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.tag == "Doodle" && other.transform.GetComponent<Rigidbody2D>().velocity.y < 1e-6){
-            anim.SetBool("Animate",true);
+        if (other.CompareTag("Doodle") && other.transform.GetComponent<Rigidbody2D>().velocity.y < 1e-6)
+        {
+            anim.SetBool("Animate", true);
+            PlayBreakingSound(); // Call the method to play sound
+        }
+    }
+
+    void PlayBreakingSound()
+    {
+        if (audioSource != null)
+        {
+            audioSource.Play(); // Play the sound
         }
     }
 }
